c375e889319617488d733535492e93f8c286a92c
Mehthab <114091862+Meht4b@users.noreply.github.com>
2025-05-27 18:23:29 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from flask import request, jsonify,session
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from config import app, db,jwt
 from models import Channels,Messages, Users
-
+import os
 import bcrypt
 
 
@@ -320,4 +320,5 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0',port=5000)
